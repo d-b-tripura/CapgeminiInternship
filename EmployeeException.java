278dc0edc808f1cf9aaf10bcb0a52cf84a509764
d-b-tripura <52556740+d-b-tripura@users.noreply.github.com>
2021-04-15 15:22:13 +0530

    Create EmployeeException.java

---
+++ EmployeeException.java
@@ -0,0 +1,22 @@
+public class EmployeeException extends Exception{
+	
+	private String excMsg = "";
+
+	public EmployeeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeException(String message) {
+		super(message);
+		this.excMsg = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee Exception :- "+excMsg;
+	}
+	
+
+
+}
